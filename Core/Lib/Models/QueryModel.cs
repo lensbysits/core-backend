2dd0fb67b9aa75b6a0ffd1dcedd05c442168704b
Cosmin Moroita <cosmin@abosolutions.nl>
2021-10-13 12:53:13 +0300

    update query model

--- Core/Lib/Models/QueryModel.cs
+++ Core/Lib/Models/QueryModel.cs
@@ -7,65 +7,73 @@ namespace Lens.Core.Lib.Models
 {
     public class QueryModel
     {
-        public int? Offset { get; set; } = 0;
-        public int? Limit { get; set; } = 50;
-
+        public int Offset { get; set; } = 0;
+        public int Limit { get; set; } = 50;
+        
+        // Filter by
         public string Tag { get; set; }
-        public string Q { get; set; }
-        public string SortField { get; set; }
-        public SortOrder? SortOrder { get; set; } = Models.SortOrder.asc;
-
         public string CreatedBy { get; set; }
         public DateTime? CreatedSince { get; set; }
         public string UpdatedBy { get; set; }
         public DateTime? UpdatedSince { get; set; }
+
+        // Search by
+        public string SearchTerm { get; set; }
+
+        // Sort by
+        public string OrderBy { get; set; }
 
         public string QueryString
         {
             get
             {
                 var queryStringSegments = new List<string>();
-                if (Offset.HasValue)
-                    queryStringSegments.Add($"offset={Offset}");
+                    
+                queryStringSegments.Add($"offset={Offset}");
+                queryStringSegments.Add($"limit={Limit}");
 
-                if (Limit.HasValue)
-                    queryStringSegments.Add($"limit={Limit}");
-
-                if (!string.IsNullOrEmpty(Tag))
+                // Filtery by
+                if (!string.IsNullOrWhiteSpace(Tag))
+                {
                     queryStringSegments.Add($"q={UrlEncoder.Default.Encode(Tag)}");
+                }
 
-                if (!string.IsNullOrEmpty(Q))
-                    queryStringSegments.Add($"q={UrlEncoder.Default.Encode(Q)}");
-
-                if (!string.IsNullOrEmpty(SortField))
-                    queryStringSegments.Add($"sortField={SortField}");
-
-                if (SortOrder.HasValue)
-                    queryStringSegments.Add($"sortOrder={SortOrder}");
-
-                if (!string.IsNullOrEmpty(CreatedBy))
+                if (!string.IsNullOrWhiteSpace(CreatedBy))
+                {
                     queryStringSegments.Add($"createdBy={CreatedBy}");
+                }
 
                 if (CreatedSince.HasValue)
+                {
                     queryStringSegments.Add($"updatedSince={CreatedSince}");
+                }
 
-                if (!string.IsNullOrEmpty(UpdatedBy))
+                if (!string.IsNullOrWhiteSpace(UpdatedBy))
+                {
                     queryStringSegments.Add($"updatedBy={UpdatedBy}");
+                }
 
                 if (UpdatedSince.HasValue)
+                {
                     queryStringSegments.Add($"updatedSince={UpdatedSince}");
+                }
 
-                if (!queryStringSegments.Any())
-                    return string.Empty;
+                // Search by
+                if (!string.IsNullOrWhiteSpace(SearchTerm))
+                {
+                    queryStringSegments.Add($"searchTerm={UrlEncoder.Default.Encode(SearchTerm)}");
+                }
+
+                // Sort by
+                if (!string.IsNullOrWhiteSpace(OrderBy))
+                {
+                    queryStringSegments.Add($"orderBy={OrderBy}");
+                }
+
+                if (!queryStringSegments.Any()) return string.Empty;
 
                 return $"?{string.Join('&', queryStringSegments)}";
             }
         }
-    }
-
-    public enum SortOrder
-    {
-        asc,
-        desc
     }
 }
