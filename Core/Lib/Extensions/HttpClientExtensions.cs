c7d00b245a96a18dd670e93328388febd03d29bf
Chesca Leonard Cornel <leonard@abosolutions.nl>
2022-01-27 15:58:03 +0200

    Add HttpClientExtensions.DeleteAsync

--- Core/Lib/Extensions/HttpClientExtensions.cs
+++ Core/Lib/Extensions/HttpClientExtensions.cs
@@ -7,6 +7,15 @@ namespace Lens.Core.Lib
 {
 	public static class HttpClientExtensions
 	{
+		public static Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest)
+		{
+			var request = new HttpRequestMessage(HttpMethod.Delete, url);
+
+			beforeRequest(request.Headers);
+
+			return httpClient.SendAsync(request);
+		}
+
 		public static Task<HttpResponseMessage> GetAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest)
 		{
 			var request = new HttpRequestMessage(HttpMethod.Get, url);
