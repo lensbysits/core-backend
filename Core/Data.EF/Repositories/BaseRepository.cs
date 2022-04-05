a60bf1d9ef172c42c180e15a67d78b8543f273d5
Johan Storteboom <johan@storteboom.net>
2022-04-05 15:12:47 +0200

    Proper abstraction and usage of BaseRepositories.

---
+++ Core/Data.EF/Repositories/BaseRepository.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper.QueryableExtensions;
+using Lens.Core.Data.EF.Entities;
+using Lens.Core.Data.Repositories;
+using Lens.Core.Lib.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Lens.Core.Data.EF.Repositories;
+
+public abstract class BaseRepository<TDbContext> : IRepository<TDbContext>
+    where TDbContext : ApplicationDbContext
+{
+    public BaseRepository(TDbContext dbContext, IApplicationService<BaseRepository<TDbContext>> applicationService) : base()
+    {
+        DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        ApplicationService = applicationService ?? throw new ArgumentNullException(nameof(applicationService));
+    }
+
+    protected TDbContext DbContext { get; }
+    protected IApplicationService<BaseRepository<TDbContext>> ApplicationService { get; }
+
+    public virtual Task<TResult> Create<TResult>(object model) { return default; }
+
+    public virtual Task<IEnumerable<TResult>> Create<TResult>(IEnumerable<object> models) { return default; }
+
+    public virtual Task<TModel> Get<TModel>(Guid id) { return default; }
+
+    public virtual Task<IEnumerable<TModel>> Get<TModel>(Expression<Func<TModel, int, bool>> predicate = null) { return default; }
+}
+
+public abstract class BaseRepository<TDbContext, TEntity>: BaseRepository<TDbContext>, IRepository
+    where TDbContext : ApplicationDbContext
+    where TEntity : class, IIdEntity
+{
+    public BaseRepository(TDbContext dbContext, IApplicationService<BaseRepository<TDbContext>> applicationService) : base(dbContext, applicationService)
+    {
+    }
+
+    public override async Task<TResult> Create<TResult>(object model)
+    {
+        var entity = ApplicationService.Mapper.Map<TEntity>(model);
+        await DbContext.AddAsync(entity);
+        await DbContext.SaveChangesAsync();
+        var result = ApplicationService.Mapper.Map<TResult>(entity);
+        return result;
+    }
+
+    public override async Task<IEnumerable<TResult>> Create<TResult>(IEnumerable<object> models)
+    {
+        var entities = models.Select(model => ApplicationService.Mapper.Map<TEntity>(model)).ToList();
+        await DbContext.AddRangeAsync(entities);
+        await DbContext.SaveChangesAsync();
+        return entities.Select(entity => ApplicationService.Mapper.Map<TResult>(entity)).ToList();
+    }
+
+    public override async Task<TModel> Get<TModel>(Guid id)
+    {
+        var result = await DbContext.Set<TEntity>()
+                                    .Where(entity => entity.Id == id)
+                                    .ProjectTo<TModel>(ApplicationService.Mapper.ConfigurationProvider)
+                                    .FirstOrDefaultAsync();
+        return result;
+    }
+
+    public override async Task<IEnumerable<TModel>> Get<TModel>(Expression<Func<TModel, int, bool>> predicate = null)
+    {
+        IQueryable<TModel> query = DbContext.Set<TEntity>()
+            .ProjectTo<TModel>(ApplicationService.Mapper.ConfigurationProvider);
+
+        if(predicate != null)
+            query.Where(predicate);
+        
+        var result = await query.ToListAsync();
+
+        return result;
+    }
+}
--- Core/Data.EF/Repositories/IRepository.cs
+++ Core/Data.EF/Repositories/IRepository.cs
@@ -1,23 +1,16 @@
 ﻿using Lens.Core.Data.EF.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
+using Lens.Core.Data.Repositories;
 
 namespace Lens.Core.Data.EF.Repositories;
 
-public interface IRepository<TDbContext> where TDbContext : ApplicationDbContext
+
+public interface IRepository<TDbContext> : IRepository
+    where TDbContext : ApplicationDbContext
 {
-    Task<TEntity> Create<TEntity>(TEntity entity);
-    Task<IEnumerable<TEntity>> Create<TEntity>(IEnumerable<TEntity> entities);
 }
 
 public interface IRepository<TDbContext, TEntity> : IRepository<TDbContext>
     where TDbContext : ApplicationDbContext
     where TEntity : class, IIdEntity
 {
-    Task<TModel> Get<TModel>(Guid id);
-    Task<IEnumerable<TModel>> Get<TModel>(Expression<Func<TEntity, int, bool>> predicate = null);
-
-    Task<TResult> CreateFromModel<TResult>(object model);
 }
