def4ee9d768140064386c320f34975c104c28d88
Johan Hoek <johanh@od.org>
2022-05-02 18:13:03 +0200

    Updated app insights extension

--- Core/Core.Lib.Azure/Extensions/LoggingExtensions.cs
+++ Core/Core.Lib.Azure/Extensions/LoggingExtensions.cs
@@ -18,22 +18,40 @@ namespace Lens.Core.Lib.Azure.Extensions
 
         public static LoggerConfiguration AddAzureAppLogging(this LoggerConfiguration loggerConfiguration, IConfiguration configuration, bool isBootstrap = false)
         {
-            var appInsightsConnectionstring = configuration.GetValue<string>("ApplicationInsights:ConnectionString");
-            
+            var appInsightsConnectionstring = configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING");
+
             if (!string.IsNullOrWhiteSpace(appInsightsConnectionstring))
             {
                 var config = TelemetryConfiguration.CreateFromConfiguration(appInsightsConnectionstring);
+                config.InstrumentationKey = ParseFromConnectionString(appInsightsConnectionstring);
+
                 return loggerConfiguration
                     .Enrich.FromLogContext()
                     .Enrich.WithOperationId()
                     .WriteTo.AzureApp()
                     .WriteTo.ApplicationInsights(
-                                config, 
-                                new OperationTelemetryConverter(), 
+                                config,
+                                new OperationTelemetryConverter(),
                                 LogEventLevel.Information); //https://oleh-zheleznyak.blogspot.com/2019/08/serilog-with-application-insights.html
             }
 
             return loggerConfiguration;
+        }
+
+        /// <summary>
+        /// Serilog doesn't seems to handle the telemetry config created from the connectionstring properly
+        /// So we need to set the deprecated instrumentation key to get serilog to work.
+        /// Sould be remove as soon as serilog also has upgraded the the application insights connection string.
+        /// </summary>
+        /// <param name="appInsightsConnectionstring">The application insights connectionstring.</param>
+        /// <returns>Instrumentation key</returns>
+        private static string ParseFromConnectionString(string appInsightsConnectionstring)
+        {
+            var parts = appInsightsConnectionstring.Split(';');
+            var part = parts.FirstOrDefault(p => p.StartsWith("InstrumentationKey="));
+            var key = part?.Split('=')[0];
+
+            return key ?? string.Empty;
         }
     }
 }
