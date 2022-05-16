e4c824eb2286dad0148a9f11787e15bf5d825b3b
Cosmin Moroita <cosmin@abosolutions.nl>
2022-05-16 12:15:19 +0300

    refactor http client methods to use nullable content

--- Core/Lib/Extensions/HttpClientExtensions.cs
+++ Core/Lib/Extensions/HttpClientExtensions.cs
@@ -7,38 +7,43 @@ namespace Lens.Core.Lib
 {
 	public static class HttpClientExtensions
 	{
-		public static Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest)
-		{
-			var request = new HttpRequestMessage(HttpMethod.Delete, url);
-
-			beforeRequest(request.Headers);
-
-			return httpClient.SendAsync(request);
-		}
-
-		public static Task<HttpResponseMessage> GetAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest)
+        public static Task<HttpResponseMessage> GetAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest, HttpContent content = null)
 		{
 			var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-			beforeRequest(request.Headers);
-
-			return httpClient.SendAsync(request);
+			return SendRequest(httpClient, beforeRequest, content, request);
 		}
 
-		public static async Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string url, HttpContent content, Action<HttpRequestHeaders> beforeRequest)
+		public static Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest, HttpContent content = null)
 		{
 			var request = new HttpRequestMessage(HttpMethod.Post, url);
-			beforeRequest(request.Headers);
-			request.Content = content;
-			return await httpClient.SendAsync(request);
+
+			return SendRequest(httpClient, beforeRequest, content, request);
 		}
 
-		public static async Task<HttpResponseMessage> PutAsync(this HttpClient httpClient, string url, HttpContent content, Action<HttpRequestHeaders> beforeRequest)
+		public static Task<HttpResponseMessage> PutAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest, HttpContent content = null)
 		{
 			var request = new HttpRequestMessage(HttpMethod.Put, url);
+
+			return SendRequest(httpClient, beforeRequest, content, request);
+		}
+
+		public static Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient, string url, Action<HttpRequestHeaders> beforeRequest, HttpContent content = null)
+		{
+			var request = new HttpRequestMessage(HttpMethod.Delete, url);
+
+			return SendRequest(httpClient, beforeRequest, content, request);
+		}
+
+		private static Task<HttpResponseMessage> SendRequest(HttpClient httpClient, Action<HttpRequestHeaders> beforeRequest, HttpContent content, HttpRequestMessage request)
+		{
 			beforeRequest(request.Headers);
-			request.Content = content;
-			return await httpClient.SendAsync(request);
+			if (content != null)
+			{
+				request.Content = content;
+			}
+
+			return httpClient.SendAsync(request);
 		}
 	}
 }
