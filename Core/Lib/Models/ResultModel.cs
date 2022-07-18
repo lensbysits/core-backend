4e2883f3b979680369c0a21f7d3e1fde0eee1038
Johan Storteboom <johan@storteboom.net>
2022-07-18 09:05:54 +0200

    Add static helper method to create a ResultModel.

--- Core/Lib/Models/ResultModel.cs
+++ Core/Lib/Models/ResultModel.cs
@@ -1,5 +1,19 @@
-﻿namespace Lens.Core.Lib.Models;
+﻿using System.Collections.Generic;
 
+namespace Lens.Core.Lib.Models;
+
+public static class ResultModel
+{
+    public static IResultModel From<T>(T value)
+    {
+        return new ResultModel<T> {  Value = value };
+    }
+
+    public static IResultModel From<T>(IEnumerable<T> value)
+    {
+        return new ResultListModel<T>(value);
+    }
+}
 public class ResultModel<TValue> : IResultModel<TValue>
 {
     public TValue Value { get; set; }
