13632f886232ab8436aa31ab453f23230d030519
Johan Storteboom <johan@storteboom.net>
2022-08-18 13:35:12 +0200

    Fix nullables and implement ClaimValues<T> method in UserContext.

--- Core/Lib/Services/IUserContext.cs
+++ Core/Lib/Services/IUserContext.cs
@@ -2,12 +2,11 @@
 
 public interface IUserContext
 {
-    string Email { get; }
-    Guid EmployeeId { get; }
-    string Username { get; }
+    string? Email { get; }
+    string? Username { get; }
 
-    T ClaimValue<T>(string claim);
-    IEnumerable<T> ClaimValues<T>(string claim);
+    T? ClaimValue<T>(string claim);
+    IEnumerable<T?>? ClaimValues<T>(string claim);
     bool HasClaim(string claim);
     bool IsInRole(string role);
 }
