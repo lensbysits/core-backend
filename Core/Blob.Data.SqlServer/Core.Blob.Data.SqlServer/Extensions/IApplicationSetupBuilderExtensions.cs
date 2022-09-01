50b8cde490a94485d899081b258137b86fc2c005
jstorteboom <johan@storteboom.net>
2022-09-01 13:38:13 +0200

    Split BlobService and BlobManagementService in their own libs (with their own extention-methods.

---
+++ Core/Blob.Data.SqlServer/Core.Blob.Data.SqlServer/Extensions/IApplicationSetupBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Lens.Core.Blob.Data;
+using Lens.Core.Blob.Services;
+using Lens.Core.Data.EF;
+using Lens.Core.Lib;
+using Lens.Core.Lib.Builders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lens.Core.Blob;
+
+public static class IApplicationSetupBuilderExtensions
+{
+    public static IApplicationSetupBuilder AddBlobManagementService(this IApplicationSetupBuilder builder, 
+        BlobImplementationType blobImplementationType,
+        string connectionStringName = "DefaultConnection",
+        string connectionStringPassword = "dbPassword")
+    {
+        builder.AddBlobService(blobImplementationType);
+
+        builder
+            .AddProgramInitializer<BlobInitializerService>()
+            .AddAssemblies(typeof(AutoMapperProfile).Assembly)
+            .AddSqlServerDatabase<BlobDbContext>(connectionStringName, connectionStringPassword)
+            .Services
+                .AddScoped<IBlobManagementService, BlobManagementService>();
+
+        return builder;
+    }
+}
