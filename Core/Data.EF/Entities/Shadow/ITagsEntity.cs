701e82640574f861d90b8aec9a0d315af6108a60
Draghici Alexandru George <dageorgero@gmail.com>
2023-02-15 11:15:03 +0200

    fixed ITagsEntity for playground, speakers apps

--- Core/Data.EF/Entities/Shadow/ITagsEntity.cs
+++ Core/Data.EF/Entities/Shadow/ITagsEntity.cs
@@ -2,8 +2,4 @@
 
 public interface ITagsEntity
 {
-    /// <summary>
-    /// This property contains multiple tags, separated by comma. Ex.: green, red, blue
-    /// </summary>
-    string? Tag { get; set; }
 }
