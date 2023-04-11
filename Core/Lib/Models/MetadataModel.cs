1b2c83d39bba15b932c23f6ac61950e1d22c04ee
jstorteboom <johan@storteboom.net>
2023-04-11 14:37:15 +0200

    Johans/md fixes (#67)
    * Handle posting an empty metadata nicely.
    * Rewrote GetMasterdata filter SP.

--- Core/Lib/Models/MetadataModel.cs
+++ Core/Lib/Models/MetadataModel.cs
@@ -8,7 +8,23 @@ public class MetadataModel : IMetadataModel
     public string? Domain { get; set; } = IMetadataModel.AllDomains;
     public JsonElement? Metadata { get; set; }
 
-    private Dictionary<string, JsonElement>? metadataDictionary;
+    private Dictionary<string, JsonElement>? _metadataDictionary;
     [JsonIgnore]
-    public Dictionary<string, JsonElement>? MetadataDictionary => metadataDictionary ??= JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(Metadata?.ToString() ?? JsonNodeUtilities.EmptyObjectJson);
+    public Dictionary<string, JsonElement>? MetadataDictionary
+    {
+        get
+        {
+            return _metadataDictionary;
+        }
+        set
+        {
+            var v = Metadata.ToString();
+            if (string.IsNullOrEmpty(v))
+            { 
+                v = JsonNodeUtilities.EmptyObjectJson; 
+            }
+
+            _metadataDictionary ??= JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(v);
+        }
+    }
 }
