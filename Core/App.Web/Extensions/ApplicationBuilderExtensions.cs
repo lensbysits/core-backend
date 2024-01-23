93f6d38f65da65b301551d19d7c5abd68c30e612
jstorteboom <johan@storteboom.net>
2024-01-23 14:05:31 +0100

    Improve Generic Grexx endpoint usage (accept GET calls with json). Test the 'get dossiers' endpoint in grexx.

--- Core/App.Web/Extensions/ApplicationBuilderExtensions.cs
+++ Core/App.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -31,7 +31,7 @@ public static class ApplicationBuilderExtensions
 
         appBuilder.UseSwagger(options =>
         {
-                options.RouteTemplate = "swagger/{documentName}/swagger.json";
+            options.RouteTemplate = "swagger/{documentName}/swagger.json";
 
             //Nintex only supports version 2 for now: https://help.nintex.com/en-US/xtensions/04_Reference/REF_KnownIssues.htm
             if (!string.IsNullOrEmpty(swaggerSettings.OpenAPIVersion) && swaggerSettings.OpenAPIVersion.Equals("2"))
@@ -48,24 +48,23 @@ public static class ApplicationBuilderExtensions
         var swaggerSettings = configuration.GetSection(nameof(SwaggerSettings)).Get<SwaggerSettings>();
         var authMethod = AuthenticationFactory.GetAuthenticationMethod(configuration);
 
-        if (swaggerSettings is null)
+        if (swaggerSettings != null)
         {
-            return appBuilder.UseSwaggerUI();
-        }
-
             appBuilder.UseSwaggerUI(options =>
             {
                 options.SwaggerEndpoint("swagger/v1/swagger.json", swaggerSettings?.AppName ?? "API V1");
 
-                foreach(var definition in swaggerSettings?.ExtraDefinitions ?? new())
+                foreach (var definition in swaggerSettings?.ExtraDefinitions ?? new())
                 {
                     options.SwaggerEndpoint($"swagger/{definition.GroupName}/swagger.json", definition.AppName);
                 }
 
                 options.RoutePrefix = string.Empty;
 
-            authMethod.UseSwaggerUI(options, swaggerSettings!);
-        });
+                authMethod.UseSwaggerUI(options, swaggerSettings!);
+            });
+        }
+
         return appBuilder;
     }
 }
